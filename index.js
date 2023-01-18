2ab0669ed49b82b432375b460de4deb21d8feeb5
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-01-18 15:12:52 -0800

    Add to current point in project, assignment I and II complete. Working on assignment III

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 //require and run our main function
+
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+const myIP = "99.199.113.162";
+
+// fetchCoordsByIP(myIP, (error, data) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:' , data);
+// });
--- iss.js
+++ iss.js
@@ -1,1 +1,52 @@
+const request = require("request");
+
 //contain logic for fetching the data from each API endpoint
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const fetchMyIP = function(callback) {
+  // use request to fetch IP address from JSON API
+  request("https://api.ipify.org?format=json", (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      return callback(Error(msg), null);
+    }
+    const result = JSON.parse(body).ip;
+    return callback(null, result);
+  });
+};
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://ipwho.is/${ip}`, (error, response, body) => {
+
+    if (error) {
+      return callback(error, null);
+    }
+
+    const parsedBody = JSON.parse(body);
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+
+    const result = {
+      latitude: parsedBody.latitude,
+      longitude: parsedBody.longitude,
+    };
+
+    return callback(null, result);
+
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
